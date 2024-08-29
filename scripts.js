700aea4888ab262ce61249b42219d167257d4f46
Pierre Haro <pierre.haroland@gmail.com>
2024-08-29 15:27:09 +0200

    Refacto code and correct typo

--- scripts.js
+++ scripts.js
@@ -34,8 +34,6 @@ const operate = (operator, firstNumber, secondNumber) => {
     return "Unknown operator";
 };
 
-
-
 const addToDisplay = (textToAdd) => {
     const displaySectionText = document.querySelector(".displaySectionText");
     const currentText = displaySectionText.textContent;
@@ -50,16 +48,21 @@ const eraseDisplay = () => {
 };
 
 const clearLastValue = () => {
-    if (lastNumber == "+" || lastNumber == "*" || lastNumber == "/" || lastNumber == "-") {
-        eraseDisplay();
+    if (isOperator(lastNumber)) {
         operatorList.pop()
-        addToDisplay(numberList[numberList.length - 1]);
         lastNumber = numberList[numberList.length - 1].slice(length - 1);
+        eraseDisplay();
+        addToDisplay(numberList[numberList.length - 1]);
     } else {
         let tempNumber = String(numberList.pop());
-        newNumber = tempNumber.split('').slice(0, tempNumber.length - 1).join();
-        lastNumber = newNumber.slice(length - 1);
-        numberList.push(newNumber + lastNumber);
+        if (tempNumber.length > 1) {
+            newNumber = tempNumber.split('').slice(0, tempNumber.length - 1).join();
+            lastNumber = newNumber.slice(length - 1);
+        } else {
+            newNumber = "";
+            lastNumber = "result"
+        }
+        numberList.push(newNumber);
         eraseDisplay();
         addToDisplay(newNumber);
     }
@@ -69,23 +72,36 @@ const isDecimal = (numberToCheck) => {
     return numberToCheck.split('').includes(".")
 };
 
+const isOperator = (letter) => {
+    if (letter == "+" || letter == "*" || letter == "/" || letter == "-") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 const addFloatingPoint = () => {
-    if (lastNumber == "+" || lastNumber == "*" || lastNumber == "/" || lastNumber == "-" || lastNumber == "result") {
+    if (isOperator(lastNumber) || lastNumber == "result") {
         numberList.push("0.");
         lastNumber = ".";
         eraseDisplay();
         addToDisplay("0.");
     } else {
         if (isDecimal(numberList[numberList.length - 1])) {
+            lastNumber = "result";
             eraseDisplay();
             addToDisplay("lmao");
-            lastNumber = "result";
         } else {
             numberList[numberList.length - 1] = numberList[numberList.length - 1] + ".";
             addToDisplay(".");
         }
     }
 };
+
+/// Add event listeners
+
+floatingPointTouch = document.querySelector("#floatingPointTouch");
+floatingPointTouch.addEventListener("click", () => addFloatingPoint());
 
 eraseButton = document.querySelector(".eraseButton");
 eraseButton.addEventListener("click", () => eraseDisplay());
@@ -94,35 +110,31 @@ backspaceButton = document.querySelector(".backspaceButton");
 backspaceButton.addEventListener("click", () => clearLastValue())
 
 touchButtons = document.querySelectorAll(".numberTouch, .operatorTouch");
-
 touchButtons.forEach((touchButton) => {
     touchButton.addEventListener("click", () => {
-        if (touchButton.querySelector(".touchText").textContent != "=" && touchButton.querySelector(".touchText").textContent != ".") {
+        let buttonTextContent = touchButton.querySelector(".touchText").textContent
+        if (buttonTextContent != "=" && buttonTextContent != ".") {
             if (touchButton.className == "numberTouch") {
                 if (lastNumber == "result") {
                     numberList = [];
                     operatorList = [];
-                    numberList.push(touchButton.querySelector(".touchText").textContent);
+                    numberList.push(buttonTextContent);
                     eraseDisplay();
-                    addToDisplay(touchButton.querySelector(".touchText").textContent);
-                    lastNumber = touchButton.querySelector(".touchText").textContent;
                 }
-                else if ((lastNumber != "+" && lastNumber != "*" && lastNumber != "/" && lastNumber != "-") && numberList.length > 0) {
-                    numberList[numberList.length - 1] = numberList[numberList.length - 1] + touchButton.querySelector(".touchText").textContent;
-                    addToDisplay(touchButton.querySelector(".touchText").textContent);
-                    lastNumber = touchButton.querySelector(".touchText").textContent;
+                else if (!(isOperator(lastNumber)) && numberList.length > 0) {
+                    numberList[numberList.length - 1] = numberList[numberList.length - 1] + buttonTextContent;
                 } else {
-                    numberList.push(touchButton.querySelector(".touchText").textContent);
+                    numberList.push(buttonTextContent);
                     eraseDisplay();
-                    addToDisplay(touchButton.querySelector(".touchText").textContent);
-                    lastNumber = touchButton.querySelector(".touchText").textContent;
                 }
+                addToDisplay(buttonTextContent);
+                lastNumber = buttonTextContent;
             } else if (touchButton.className == "operatorTouch") {
+                operatorList.push(buttonTextContent);
                 eraseDisplay();
-                addToDisplay(touchButton.querySelector(".touchText").textContent);
-                operatorList.push(touchButton.querySelector(".touchText").textContent);
-                lastNumber = touchButton.querySelector(".touchText").textContent
             }
+            addToDisplay(buttonTextContent);
+            lastNumber = buttonTextContent
         }
     });
 });
@@ -133,23 +145,16 @@ operateTouch.addEventListener("click", () => {
         window.alert("Error, please enter an operation before pressing =")
     } else {
         result = operate(operatorList[0], numberList[0], numberList[1]);
+        eraseDisplay();
         if (isNaN(result)) {
-            eraseDisplay();
             addToDisplay("lmao");
-            lastNumber = "result";
         } else {
-            roundedResult = Math.round(result * 100000) / 100000;
-            eraseDisplay();
-            addToDisplay(roundedResult);
             numberList = numberList.slice(2);
             operatorList = operatorList.slice(1);
+            roundedResult = Math.round(result * 100000) / 100000;
             numberList.unshift(roundedResult);
-            lastNumber = "result";
+            addToDisplay(roundedResult);
         }
+        lastNumber = "result";
     }
-});
-
-floatingPointTouch = document.querySelector("#floatingPointTouch");
-floatingPointTouch.addEventListener("click", () => {
-    addFloatingPoint();
 });
